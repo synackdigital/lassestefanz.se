a0fa20daa1ba1e3b012e89df8af3de1772f546aa
frebro <frebro@gmail.com>
2013-03-14 12:11:20 +0100

    home page uses custom query

--- wordpress/wp-content/themes/lasse-stefanz/includes/theme.php
+++ wordpress/wp-content/themes/lasse-stefanz/includes/theme.php
@@ -35,7 +35,13 @@ add_action('init', 'ls_setup_images');
 add_filter('lc_album_image_size', function() { return LS_ALBUM_IMAGE_SIZE; });
 add_filter('ls_campaign_image_size', function() { return LS_CAMPAIGN_IMAGE_SIZE; });
 
-
+function ls_home_query($query)
+{
+    if ((is_home() || is_front_page()) && $query->is_main_query()) {
+        $query->set( 'posts_per_page', '3' );
+    }
+}
+add_action( 'pre_get_posts', 'ls_home_query' );
 
 function ls_setup_hero()
 {
