763e7d59dc81fc4b7baef44de6987feff9d3cc64
Simon Fransson <simon@dessibelle.se>
2013-03-14 15:58:28 +0100

    Removed JS files

--- wordpress/wp-content/themes/lasse-stefanz/js/lasse-stefanz.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-
-    $('#hero .wooslider li').each(function() {
-        $(this).find('.slide-content').css({
-            'background-image': 'url(' + $(this).data('thumb') + ')'
-        });
-    });
-
-});
