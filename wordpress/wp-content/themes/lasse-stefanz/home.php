25aa015d6d7002963da908733815f14e105b0f8f
frebro <frebro@gmail.com>
2013-04-03 20:18:52 +0200

    gigs displayed on front page

--- wordpress/wp-content/themes/lasse-stefanz/home.php
+++ wordpress/wp-content/themes/lasse-stefanz/home.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
+    <aside id="gigs">
+        <h1>Kommande spelningar</h1>
+        <?php
+          echo do_shortcode('[eo_events event_start_after="today" showpastevents=false]<time>%start{j F}% kl %start{G:i}%</time> &middot; <a class="venue" href="%event_url%">%event_venue%</a>[/eo_events]');
+        ?>
+    </aside>
+
     <div id="container"<?php echo hobo_class_for_element_id('container'); ?>>
         <div id="content"<?php echo hobo_class_for_element_id('content'); ?>>
             <?php get_template_part('loop', 'home'); ?>
