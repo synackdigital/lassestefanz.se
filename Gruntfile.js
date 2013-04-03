4800ddb969cb375734792eed40ee12d8666a4d79
frebro <frebro@gmail.com>
2013-04-03 14:53:02 +0200

    moved node_modules and Gruntfile to root directory and introduced Bower for Bootstrap

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+    // Paths
+    var cmp_src    = 'components/';
+    var js_src     = 'wordpress/wp-content/themes/lasse-stefanz/assets/js/';
+    var js_dest    = 'wordpress/wp-content/themes/lasse-stefanz/js/';
+    var css_src    = 'wordpress/wp-content/themes/lasse-stefanz/assets/less/';
+    var css_dest   = 'wordpress/wp-content/themes/lasse-stefanz/';
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // LESS task
+        less: {
+            main: {
+                // options: {
+                //     yuicompress: true
+                // },
+                files: {
+                    "wordpress/wp-content/themes/lasse-stefanz/style.css" : 'wordpress/wp-content/themes/lasse-stefanz/assets/less/style.less'
+                }
+            }
+        },
+
+        // Concat task
+        concat: {
+            options: {
+                separator: ';'
+            },
+            main: {
+                src: [
+                    js_src+'main.js'
+                ],
+                dest: js_dest+'<%= pkg.name %>.js'
+            }
+        },
+
+        // Uglify task
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                        ' * <%= pkg.website %>/\n' +
+                        ' * Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+                        '<%= pkg.author %>; <%= pkg.license %> */\n'
+            },
+            main: {
+                src: js_dest+'<%= pkg.name %>.js',
+                dest: js_dest+'<%= pkg.name %>.min.js'
+            }
+        },
+
+        // Watch task
+        watch: {
+            main: {
+                files: [js_src+'*.js', css_src+'*.less'],
+                tasks: ['default'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    // Default task
+    grunt.registerTask('default', ['less', 'concat', 'uglify']);
+
+};
