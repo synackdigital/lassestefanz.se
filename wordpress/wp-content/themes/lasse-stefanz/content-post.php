ea8b38e5b7bf528b5b9ce64170e4570727a61a7f
frebro <frebro@gmail.com>
2013-04-29 13:08:40 +0200

    content styling

--- wordpress/wp-content/themes/lasse-stefanz/content-post.php
+++ wordpress/wp-content/themes/lasse-stefanz/content-post.php
@@ -1,10 +1,6 @@
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <h2 class="entry-title"><?php if (!is_single()) : ?><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'hobo' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php endif; ?><?php the_title(); ?><?php if (!is_single()) : ?></a><?php endif; ?></h2>
 
-        <div class="entry-meta">
-            <?php hobo_posted_on(); ?>
-        </div><!-- .entry-meta -->
-
 <?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
         <div class="entry-summary">
             <?php the_excerpt(); ?>
@@ -17,22 +13,7 @@
 <?php endif; ?>
 
         <div class="entry-utility">
-            <?php if ( count( get_the_category() ) ) : ?>
-                <span class="cat-links">
-                    <?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'hobo' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-                </span>
-            <?php endif; ?>
-            <?php
-                $tags_list = get_the_tag_list( '', ', ' );
-                if ( $tags_list ):
-            ?>
-                <span class="meta-sep">|</span>
-                <span class="tag-links">
-                    <?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'hobo' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
-                </span>
-                <span class="meta-sep">|</span>
-            <?php endif; ?>
-            <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'hobo' ), __( '1 Comment', 'hobo' ), __( '% Comments', 'hobo' ) ); ?></span>
+            <?php hobo_posted_on(); ?>
             <?php edit_post_link( __( 'Edit', 'hobo' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
         </div><!-- .entry-utility -->
     </article><!-- #post-## -->
