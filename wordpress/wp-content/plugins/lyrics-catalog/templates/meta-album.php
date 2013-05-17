a1e8eb91657b5f67fee94d3487dfe7530add37fa
Simon Fransson <simon@dessibelle.se>
2013-05-17 15:25:46 +0200

    Minor responsive adjustments etc.

--- wordpress/wp-content/plugins/lyrics-catalog/templates/meta-album.php
+++ wordpress/wp-content/plugins/lyrics-catalog/templates/meta-album.php
@@ -1,5 +1,3 @@
-<?php lc_album_cover(null, array('link' => 'image')); ?>
-
 <?php
 
 $year = lc_album_year();
@@ -10,23 +8,34 @@ $player = lc_soundcloud_player();
 
 ?>
 
+<div class="album-content">
+
+    <?php lc_album_cover(null, array('link' => 'image')); ?>
+
+    <dl class="album-meta">
+        <?php if ($year): ?>
+            <dt class="album-meta album-year"><?php _e('Year', 'lyrics-catalog'); ?></dt><dd><?php echo $year; ?></dd>
+        <?php endif; ?>
+
+        <?php if ($label): ?>
+            <dt class="album-meta album-label"><?php _e('Label', 'lyrics-catalog'); ?></dt><dd><?php echo $label; ?></dd>
+        <?php endif; ?>
+
+        <?php if ($formats): ?>
+            <dt class="album-meta album-formats"><?php _e('Formats', 'lyrics-catalog'); ?></dt><dd><?php echo $formats; ?></dd>
+        <?php endif; ?>
+
+    </dl>
+</div>
+
 
 <?php if ($tracks): ?>
-    <h3 class="album-track-list-heading"><? _e('Track listing', 'lyrics-catalog'); ?></h3>
-    <?php echo $tracks; ?>
+    <div class="album-track-list">
+        <h3 class="album-track-list-heading"><? _e('Track listing', 'lyrics-catalog'); ?></h3>
+        <?php echo $tracks; ?>
+    </div>
 <?php endif; ?>
 
-<?php if ($year): ?>
-    <p class="album-meta album-year"><strong><?php _e('Year', 'lyrics-catalog'); ?></strong>: <?php echo $year; ?></p>
-<?php endif; ?>
-
-<?php if ($label): ?>
-    <p class="album-meta album-label"><strong><?php _e('Label', 'lyrics-catalog'); ?></strong>: <?php echo $label; ?></p>
-<?php endif; ?>
-
-<?php if ($formats): ?>
-    <p class="album-meta album-formats"><strong><?php _e('Formats', 'lyrics-catalog'); ?></strong>: <?php echo $formats; ?></p>
-<?php endif; ?>
 
 <?php if ($player) : ?>
     <?php echo $player; ?>
